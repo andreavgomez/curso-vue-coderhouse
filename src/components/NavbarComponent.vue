12cba50117998058fb110168c15b6c2ea1b3f2b1
Andrea Gomez <andrea.vgomez27@gmail.com>
2023-07-01 00:42:06 -0300

    pre entrega 2

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -3,13 +3,17 @@
     <!-- Navigation -->
     <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
       <div class="container">
-        <a class="navbar-brand float-left" href="https://www.google.com" target="_blank">
+        <a class="navbar-brand float-left" href="/" target="_blank">
+        <!-- <a class="navbar-brand float-left" href="https://www.google.com" target="_blank"> -->
+        <!-- <router-link to="/">Home</router-link> -->
           Libreria El arte de Leer
         </a>
         <ul class="nav navbar-nav flex-row float-right inline-block">
           <li class="nav-item">
             <!-- <router-link class="btn btn-outline-primary" v-if="!authenticated" to="/login">Ingresar</router-link> -->
-            <router-link class="btn btn-outline-primary" to="/login">Ingresar</router-link>
+            <!-- <router-link class="btn btn-outline-primary" to="/login">Ingresar</router-link> -->
+            <a href="/login" target="_blank"><i class="bi bi-person-circle bi-size"></i></a>
+            
           </li>
           <li class="nav-item">
             <!-- <router-link class="btn btn-outline-danger" v-if="authenticated" to="/about" -->
@@ -21,7 +25,8 @@
             <router-link class="btn btn-outline-primary" to="/signup">Registrarse</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="btn btn-outline-primary" to="/carrito">carrito</router-link>
+            <!-- <router-link class="btn btn-outline-primary" to="/carrito">carrito</router-link> -->        
+            <a href="/carrito" target="_blank"><i class="bi bi-cart bi-size"></i></a>
           </li>          
         </ul>
       </div>
@@ -51,3 +56,11 @@
     }    
 }
 </script>
+
+<style>
+    .bi-size {
+        font-size: 30px; /* Ajusta el tamaño del icono según tus necesidades */
+        width: 2rem;
+        
+    }
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <p>Soy home page</p>
+    <!-- <p>Soy home page</p> -->
     <div class="row">
       <div class="col" v-for="libro in libros" :key="libro.id">
         <div :id="libro.id" class="card" style="width: 18rem;">
           <img class="card-img-top img-fluid" style="height: 200px;" src="../assets/libroNuevo.jpg" :alt="libro.titulo">
+          <!-- <img class="card-img-top img-fluid" style="height: 200px;" :src="libro.imagen" :alt="libro.titulo">           -->
           <div class="card-body">
             <h4 class="card-title">{{libro.titulo}}</h4>
             <p>Autor: {{libro.autor}}</p>
